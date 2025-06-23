e7cbe8a5908929abdbf5b01989c4b8590c671170
beno1234 <benolopesdias@gmail.com>
2025-06-23 14:13:58 -0300

    email

--- src/app/home/Header.tsx
+++ src/app/home/Header.tsx
@@ -69,7 +69,7 @@ const Header = () => {
                 <Image
                   src={src}
                   alt={`Projeto ${index + 1}`}
-                  width={src === "/carousel2/see-logo.png" ? 120 : 180}
+                  width={src === "/carousel2/see-logo.png" ? 100 : 180}
                   height={80}
                   className="object-contain"
                 />
