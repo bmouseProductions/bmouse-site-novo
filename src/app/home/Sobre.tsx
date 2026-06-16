80bf9e087c9861a398574fcb9e9234657c26a18e
bmouseProductions <137824606+bmouseProductions@users.noreply.github.com>
2026-06-16 20:58:07 -0300

    Add files via upload

--- src/app/home/Sobre.tsx
+++ src/app/home/Sobre.tsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 
 const projetos = [
   {
+    src: "/projetos/casa-sobre-a-rocha.png",
+    alt: "A Caso Sobre a Rocha - Juliano Cazarré",
+    slug: "a-casa-sobre-a-rocha",
+    cols: "md:col-span-5 md:row-span-2 row-span-1 col-span-3",
+  },
+  {
     src: "/projetos/cury/cury-desk.png",
     alt: "The most read psichiatrist in the world",
     slug: "augusto-cury",
